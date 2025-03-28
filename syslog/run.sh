2829c9bfb636b4a699c0d71823f9809040344c53
Alfredo Cardigliano <cardigliano@ntop.org>
2025-03-28 18:59:00 +0100

    Fix params

--- syslog/run.sh
+++ syslog/run.sh
@@ -2,9 +2,9 @@
 
 if [ -z "$1" ] || [ -z "$2" ]; then
     echo "Usage: $0 <ntopng IP> <ntopng syslog port>"
-    echo "Example: $0 127.0.0.1 9999"
+    echo "Example: $0 127.0.0.1 5140"
 fi
 
 TODAY=$(date +'%Y-%m-%d')
 
-cat suricata.log | sed -e "s:2022-06-11:${TODAY}:g" | netcat 127.0.0.1 9999
+cat suricata.log | sed -e "s:2022-06-11:${TODAY}:g" | netcat $1 $2
